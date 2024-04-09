376eba6eb6239b387b4f93f95e27265e8c655210
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-09 10:06:57 -0600

    Add function to delete columns that start with X

--- 2024_04_08_Monthly_Expenses.R
+++ 2024_04_08_Monthly_Expenses.R
@@ -11,8 +11,32 @@ View(new_expenses)
 
 # EDA exploratory data analysis
 #view first few lines, or can do tail(my_data)
-head(expenses)
+head(new_expenses)
 #summary
-summary(expenses)
+summary(new_expenses)
 #explore structure
-str(expenses)
+str(new_expenses)
+
+# Remove columns that start with X
+# Function to remove columns starting with "X"
+remove_columns_starting_with_X <- function(new_expenses) {
+  # Get column names
+  column_names <- names(new_expenses)
+  
+  # Check if any column starts with "X"
+  if (any(grepl("^X", column_names))) {
+    # If true, remove columns starting with "X"
+    cleaned_data <- new_expenses[, !grepl("^X", column_names)]
+    return(cleaned_data)
+  } else {
+    # If false, return original data frame
+    return(new_data)
+  }
+}
+
+# Apply the function
+cleaned_df <- remove_columns_starting_with_X(new_expenses)
+
+# View cleaned data frame
+print(cleaned_df)
+View(cleaned_df)
